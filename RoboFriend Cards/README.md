3a35cccb6e47151e6776488c376ba6d393d56027
Zack Fons <nofcaz@protonmail.com>
2023-11-16 17:04:58 -0500

    Added old project files

---
+++ RoboFriend Cards/README.md
@@ -0,0 +1,13 @@
+# Robo-Friends React Application
+
+This React application allows the user to search through a collection of "Robo-Friends" cards, each with a different name and character portrait attached to them. Once certain letters are typed into the input box, the collection of cards will reduce until only those that match the user's input remain.
+
+This project was my first with the React library and it was enthralling because of how simplistic and yet interconnected it was overall. From managing "this" properties to destructuring variables and gaining valuable, practical experience with manipulating JSON, I was able to find a new appreciation for tools outside of the typical HTML/CSS/JavaScript. What I found most interesting was controlling "state," a concept I'd read about previously but never worked with first-hand, and through which I experienced several unexpected but welcome "Aha!" moments.
+
+## Main page:
+
+![Main](https://i.imgur.com/MlNFus8.png?1 'Main')
+
+## Searcing for a specific robot:
+
+![Search](https://i.imgur.com/HKttHKg.png?1 'Search')
---
+++ RoboFriend Cards/src/containers/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			Robots: [],
+			searchfield: ''
+		}
+	}
+
+componentDidMount() {
+	fetch('https://jsonplaceholder.typicode.com/users')
+	.then(response => response.json())
+	.then(users => this.setState({ Robots: users }));
+}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value })
+	}
+
+	render() {
+		const { Robots, searchfield } = this.state;
+		const filteredRobots = Robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+		})
+		return !Robots.length ?
+		<h1>Loading androids...</h1> :
+		(
+					<div className="tc">
+						<h1>RoboFriends</h1>
+						<SearchBox SearchChange={this.onSearchChange} />
+						<Scroll>
+							<CardList Robots={filteredRobots} />
+						</Scroll>
+					</div>
+				)
+			}
+}
+
+export default App;
